52996e4359599aae4c094764c53e419693b47436
MooreDA <31998413+MooreDA@users.noreply.github.com>
2017-09-22 21:29:01 -0500

    Add files via upload

---
+++ Explicación de variables/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(" Bienvenidos a Java ");
+
+	    //Enteros
+        byte a;
+        short b;
+        int c = 27;
+        long d;
+
+        //Decimales
+        float e;
+        double f = 0.0;
+
+        // Texto
+        char g = 'x';
+        String i = " x ";
+
+        //Boleano
+        boolean vivo = true;
+        vivo = false;
+    }
+}
